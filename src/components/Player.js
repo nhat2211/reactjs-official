37031665c3223141ba81224adb3bcc0d4317fa0d
nhat2211 <tranquangnhat2211@gmail.com>
2023-09-28 14:15:54 +0700

    add react component part 2

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,83 +1,48 @@
-import React,  {Component} from "react";
+import React, { Component } from "react";
+
+import Players from "../shared/Players"
 
 export default class Player extends Component {
     render() {
         return (
-           
+
             <div className="container player">
-                <div className="row row-content">
+                 <div className="row row-content">
+                 {Players.map((player) => (
+                       
+                            <div className="col col-sm-4 col-md-4" key={player.id}>
+                                <div className="card">
+                                    <img src={player.img} />
+                                    <h3>{player.name}</h3>
+                                    <p className="title">{player.club}</p>
+                                    <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
+                                </div>
+                            </div>
 
-                <div className="col col-sm-4 col-md-4">
-                    <div className="card">
-                        <img src='assets/images/cr.jpg'/>
-                        <h3>Cristiano Ronaldo</h3>
-                        <p className="title">Manchester United</p>
-                        <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
-                    </div>
-                </div>
+                        
+                      
 
-                <div className="col col-sm-4 col-md-4">
-                    <div className="card">
-                        <img src='assets/images/m.jpg'/>
-                        <h3>Kylaa Mpappe</h3>
-                        <p className="title">PSG</p>
-                        <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
-                    </div>
-                </div>
+        
 
-                <div className="col col-sm-4 col-md-4">
-                    <div className="card">
-                        <img src='assets/images/haaland.jpg'/>
-                        <h3>haaland</h3>
-                        <p className="title">Manchester City</p>
-                        <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
-                    </div>
-                </div>
+                   
 
 
 
-                </div>
 
-                <div className="row row-content">
 
-                <div className="col col-sm-4 col-md-4">
-                    <div className="card">
-                        <img src='assets/images/cr.jpg'/>
-                        <h3>Cristiano Ronaldo</h3>
-                        <p className="title">Manchester United</p>
-                        <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
-                    </div>
-                </div>
-
-                <div className="col col-sm-4 col-md-4">
-                    <div className="card">
-                        <img src='assets/images/m.jpg'/>
-                        <h3>Kylaa Mpappe</h3>
-                        <p className="title">PSG</p>
-                        <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
-                    </div>
-                </div>
-
-                <div className="col col-sm-4 col-md-4">
-                    <div className="card">
-                        <img src='assets/images/haaland.jpg'/>
-                        <h3>haaland</h3>
-                        <p className="title">Manchester City</p>
-                        <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
-                    </div>
-                </div>
+                ))}
 
 
 
-                </div>
+                 </div>
+               
 
-                
-                
 
 
             </div>
         )
-    }
 
-   
-} 
+
+
+    }
+}
---
+++ src/shared/Players.js
@@ -0,0 +1,12 @@
+
+const Players=[
+    {id:1,name:"Cristiano Ronaldo", club:"MU",img:"assets/images/cr.jpg"},
+    {id:2,name:"Kylaa Mpappe", club:"MU",img:"assets/images/m.jpg"},
+    {id:3,name:"Haaland", club:"MU",img:"assets/images/haaland.jpg"},
+    {id:4,name:"Cristiano Ronaldo", club:"MU",img:"assets/images/cr.jpg"},
+    {id:5,name:"Kylaa Mpappe", club:"MU",img:"assets/images/m.jpg"},
+    {id:6,name:"Haaland", club:"MU",img:"assets/images/haaland.jpg"}
+
+];
+
+export default Players;
